4e4045685b7d62abe5e7cfc5a40b1c938435abde
Zain ul din Zafar <104352655+zain-ul-din-zafar@users.noreply.github.com>
2024-08-10 18:41:13 +0500

    Update Firebase.ts

--- src/lib/Firebase.ts
+++ src/lib/Firebase.ts
@@ -2,7 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GithubAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import { getFirestore, collection, doc, setDoc } from 'firebase/firestore';
 
-const firebaseConfig = {};
+const firebaseConfig = {
+    apiKey: "AIzaSyAAEkvETCjXie0RTaYETHwF_hPpyC2u1C4",
+    authDomain: "linklift-75e4b.firebaseapp.com",
+    projectId: "linklift-75e4b",
+    storageBucket: "linklift-75e4b.appspot.com",
+    messagingSenderId: "299526570257",
+    appId: "1:299526570257:web:7cf5fac54743f23569ccb0",
+    measurementId: "G-GQ8GH86YK8"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
